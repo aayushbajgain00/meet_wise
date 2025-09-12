f4925b7ed5ef0f5dd1cb1de59337019343991d7a
Senbonzakura10 <168708258+aayushbajgain00@users.noreply.github.com>
2025-09-12 23:46:36 +1000

    zoom Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,11 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import BotRoutes from "./routes/botRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
+import { fileURLToPath } from "url";
+import path from "path"; 
+import webhookRoutes from "./routes/webhooks.js";
+
+
 
 
 // const {createBot} = require("../backend/controller/botcontroller")
@@ -15,9 +20,22 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
+// Webhooks (Zoom)
+app.use("/webhooks", webhookRoutes);
+
+// basic health
+app.get("/health", (req, res) => res.json({ ok: true }));
+
+// serve local recordings in dev (DON'T expose in prod)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(
+  "/recordings",
+  express.static(path.join(__dirname, process.env.RECORDINGS_DIR || "recordings"))
+);
+
 
 // Example route
-
 const MongoURI="mongodb+srv://985173_db_user:k09rX6a7JiqQAZyd@meetwise.sstjbaa.mongodb.net/?retryWrites=true&w=majority&appName=MeetWise"
 // const conn = await mongoose.connect(MangoURI, {
 //     useNewUrlParser: true,
@@ -31,9 +49,8 @@ app.use("/bots", BotRoutes)
 app.use("/api/user", userRoutes);
 
 // MongoDB connection
-await mongoose.connect(MongoURI)
+mongoose.connect(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
   app.listen(5000, () => console.log("Server running on http://localhost:5000"));
 })
 .catch(err => console.log(err));
-
