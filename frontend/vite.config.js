6f7a11c3bd2f2a32b31456b93fe35e3caa1a11be
Senbonzakura10 <168708258+aayushbajgain00@users.noreply.github.com>
2025-09-18 01:27:50 +1000

    Error solved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+<<<<<<< Updated upstream
   plugins: [react()],
+=======
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  server: {
+    port: 3000
+  }
+>>>>>>> Stashed changes
 })
