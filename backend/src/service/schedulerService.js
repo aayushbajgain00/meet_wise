945bb89099694bb270924528ed7098491d05c7aa
timsinamandip33-stack <timsinamandip33@gmail.com>
2025-09-24 21:21:08 +0930

    latest_aayush

---
+++ backend/src/service/schedulerService.js
@@ -0,0 +1,31 @@
+// src/scheduler.js
+import cron from "node-cron";
+import ScheduledMeeting from "../model/schedule.js";
+import { joinZoomMeeting } from "../utils/ZoomPuppet.js";
+
+cron.schedule("* * * * *", async () => {
+  const now = new Date();
+  const dueMeetings = await ScheduledMeeting.find({
+    autoJoin: true,
+    scheduledTime: { $lte: now },
+    status: "scheduled",
+  });
+
+  for (const m of dueMeetings) {
+    try {
+      console.log(`⏰ Time to join meeting ${m.meetingId}`);
+      await joinZoomMeeting(m.meetingId, m.password);
+        console.log("👉 Calling joinZoomMeeting now...");
+      // await joinZoomMeeting("987654321", "xyz789");
+      console.log("👉 joinZoomMeeting finished/returned");
+      m.status = "joining";
+      await m.save();
+    } catch (err) {
+      console.error("❌ Bot failed to join", err.message);
+      m.status = "failed";
+      await m.save();
+    }
+  }
+});
+
+console.log("📅 Scheduler service started...");
