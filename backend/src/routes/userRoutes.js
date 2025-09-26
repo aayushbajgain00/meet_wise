5775d27356caa8c454fe050c1f5c678642906fc4
Basnetbhojraj <emailbrb0@gmail.com>
2025-09-26 18:26:05 +1000

    Add profile retrieval and account management routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,6 +4,10 @@ import {
   registerUser,
   loginUser,
   microsoftLogin,
+  updateProfile,
+  getProfile,
+  changePassword,
+  deleteAccount,
 } from "../controller/usercontroller.js";
 import {
   startGoogleOAuth,
@@ -19,6 +23,10 @@ router.put("/profile", updateProfile);
 router.post("/microsoft-login", microsoftLogin);
 router.get("/google", startGoogleOAuth);
 router.get("/google/callback", handleGoogleOAuthCallback);
+router.get("/profile", getProfile); // Add this route
 
+// Account settings routes
+router.put("/account/password", changePassword);
+router.delete("/account", deleteAccount);
 
 export default router;
